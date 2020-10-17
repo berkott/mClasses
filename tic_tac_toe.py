9aeadcb72df1a9b4e4a3f4ec61b2685d64a48fef
Berkan Ottlik <berkott3002@gmail.com>
2020-10-17 14:13:44 -0400

    tic tac toe final changes

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,17 +15,13 @@ board = [[" ", " ", " "],
 
 def check_win():
   horizontal_victory = board[row][0] == player_turn and board[row][1] == player_turn and board[row][2] == player_turn
-
   vertical_victory = board[0][col] == player_turn and board[1][col] == player_turn and board[2][col] == player_turn
-
   diagonal_neg_victory = board[0][0] == player_turn and board[1][1] == player_turn and board[2][2] == player_turn
-
   diagonal_pos_victory = board[0][2] == player_turn and board[1][1] == player_turn and board[2][0] == player_turn
 
   return horizontal_victory or vertical_victory or diagonal_neg_victory or diagonal_pos_victory
 
-while not game_over:
-  # Print Board
+def print_board():
   print("  0 1 2")
   print(f"0 {board[0][0]}|{board[0][1]}|{board[0][2]}")
   print("  -+-+-")
@@ -33,7 +29,7 @@ while not game_over:
   print("  -+-+-")
   print(f"2 {board[2][0]}|{board[2][1]}|{board[2][2]}")
 
-  # Get User Input
+def get_input():
   while True:
     row = input("Please input a row: ")
     col = input("Please input a column: ")
@@ -41,7 +37,8 @@ while not game_over:
     if row.isdigit() and col.isdigit():
       row = int(row)
       col = int(col)
-      if row <= 2 and row >= 0 and col <= 2 and col >= 0:
+      # if row <= 2 and row >= 0 and col <= 2 and col >= 0:
+      if 0 <= row <= 2 and 0 <= col <= 2:
         if board[row][col] == " ":
           break
         else:
@@ -49,7 +46,16 @@ while not game_over:
       else:
         print("Please input a row and col of 0, 1, or 2")
     else:
-      print("Please input and integer for both the row and column")
+      print("Please input an integer for both the row and column")
+    
+  return row, col
+
+while not game_over:
+  # Print Board
+  print_board()
+
+  # Get User Input
+  row, col = get_input()
 
   board[row][col] = player_turn
 
@@ -57,12 +63,7 @@ while not game_over:
   if check_win():
     game_over = True
     print(f"Player {player_turn} Won!")
-    print("  0 1 2")
-    print(f"0 {board[0][0]}|{board[0][1]}|{board[0][2]}")
-    print("  -+-+-")
-    print(f"1 {board[1][0]}|{board[1][1]}|{board[1][2]}")
-    print("  -+-+-")
-    print(f"2 {board[2][0]}|{board[2][1]}|{board[2][2]}")
+    print_board()
 
   # Change Turn
   if player_turn == "X":
